11b03908b6284512c758d5f21d825bf47fba6a91
CoffeeBeans007 <89408501+CoffeeBeans007@users.noreply.github.com>
2023-02-20 03:22:50 +0100

    Add files via upload

--- Electricity/main.py
+++ Electricity/main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import time
 import multiprocessing as mp
 import logging
+import re
 
 
 ##### Variables to share between processes #####
@@ -24,42 +25,147 @@ def casedetails(app):
         return(info)
 previous_news=None
 temperaturelog={}
-def returnDay(stringNews: str):
-    days = {"DAY 1", "DAY 2", "DAY 3", "DAY 4", "DAY 5"}
-    for day in days:
-        if day in news["headline"]:
-            givenday=day
-            break
+class textAnalysis():
+    def returnDay(string:str):
+        days = {"DAY 1", "DAY 2", "DAY 3", "DAY 4", "DAY 5"}
+        for day in days:
+            if day in string.capitalize():
+                givenday=day
+                break
+            else:
+                givenday="None"
+        return givenday
+    def returnType(stringElement):
+        if "temperature forecast" in stringElement.lower():
+            return("Temp")
+        elif "fines" == stringElement.lower():
+            return("Fine")
+        elif "spot price and volumes" in stringElement.lower():
+            return("SpotVol")
+        elif "sunlight forecast" in stringElement.lower():
+            return("LightForecast")
+        elif "price and volume bulletin" in stringElement.lower():
+            return("VolumeBul")
+        elif "sunlight" in stringElement.lower():
+            return("SunlightFor")
+       
+        
+        
+    
+    def returnTwoNum(phrase,target_string):
+       
+        matches = re.findall(r'{}.*?([\d.]+).*?([\d.]+)'.format(target_string), phrase)
+        return matches
+       
+    def returnTwoNums(phrase,target_string,str2):
+        if target_string in phrase:
+            matches = re.findall(r'{}.*?([\d.]+).*?([\d.]+)'.format(target_string), phrase)
+            
         else:
-            givenday=None
-    return givenday
-def returnType(stringElement):
-    if "TEMPERATURE Forecast" in stringElement:
-        return("Temp")
-    elif "FINES" == stringElement:
-        return("Fine")
-    elif "SPOT PRICE AND VOLUMES" in stringElement:
-        return("SpotVol")
-    elif ""
+            matches=textAnalysis.returnWordBefore(phrase,1,str2)
+        return matches
+
+    def returnWordBefore(my_phrase,order,line):
+
+        match = re.search(r'(\d+(?:\.\d+)?)\D+(?={})'.format(line), my_phrase)
+        return(match.group(order))
+    def get_decimal_after_word(phrase, word):
+       
+        
+        pos = phrase.find(word)
+        if pos == -1:
+            return None
+
+    
+        substring = phrase[pos+len(word):]
+
+        
+        decimal_str = re.search(r'\d+\.\d+', substring)
+        if not decimal_str:
+            return None
+        decimal = float(decimal_str.group())
+
+        return decimal
+
 def newsInfoFounder(news: list):
-    #Return given day
+    # create an empty dictionary to hold the information we'll extract from the news list
+    InfoDic = {}
+
+    # loop through each news item in the list
+    for j in news:
+        # determine the type of news item based on its headline
+        typeReturn = textAnalysis.returnType(j["headline"])
+
+        # if the news item is about temperature
+        if typeReturn == "Temp":
+            # extract the temperature and tick information from the news item's body
+            # and add it to the InfoDic dictionary with a key that includes the day and tick information
+            InfoDic["Temp " + textAnalysis.returnDay(j["headline"]) + " at tick " + str(j["tick"])] = (
+                textAnalysis.returnTwoNum(j["body"], "temperature"), j["tick"])
+
+        # if the news item is about fines
+        elif typeReturn == "Fine":
+            # extract the fine amount and tick information from the news item's body
+            # and add it to the InfoDic dictionary with a key that includes the day and tick information
+            InfoDic["Fines " + str(textAnalysis.returnDay(j["body"])) + " at tick " + str(j["tick"])] = (
+                textAnalysis.returnTwoNum(j["body"], "fine")[0], j["tick"])
+
+        # if the news item is about volume and price movements for a particular commodity
+        elif typeReturn == "VolumeBul":
+            # extract the volume information and tick information from the news item's body
+            # as well as other information about the contracts and participants
+            # and add it to the InfoDic dictionary with a key that includes the day and tick information
+            InfoDic["VolumeBul" + textAnalysis.returnDay(j["headline"]) + " at tick" + str(j["tick"])] = [
+                textAnalysis.returnTwoNum(j["body"], "between"),
+                j["tick"],
+                textAnalysis.returnWordBefore(j["body"], 1, "contracts for buying"),
+                textAnalysis.returnWordBefore(j["body"], 1, "contracts for selling"),
+                textAnalysis.returnWordBefore(j["body"], 1, "Producers"),
+                textAnalysis.returnWordBefore(j["body"], 1, "Distributors"),
+                textAnalysis.returnWordBefore(j["body"], 1, "Traders"),
+                float(textAnalysis.returnWordBefore(j["body"], 1, "cent")) / 100
+            ]
+
+        # if the news item is about spot volume and price movements for a particular commodity
+        elif typeReturn == "SpotVol":
+            # extract the spot volume and price information and tick information from the news item's body
+            # and add it to the InfoDic dictionary with a key that includes the day and tick information
+            InfoDic["SpotVol " + str(textAnalysis.returnDay(j["headline"])) + " at tick " + str(j["tick"])] = (
+                textAnalysis.returnWordBefore(j["body"], 1, "to buy")[0],
+                textAnalysis.get_decimal_after_word(j["body"], "buy at a price"),
+                textAnalysis.returnWordBefore(j["body"], 1, "to sell")[0],
+                textAnalysis.get_decimal_after_word(j["body"], "sell at a price"),
+                j["tick"]
+            )
+
+        # if the news item is about sunlight forecast
+        elif "SUNLIGHT Forecast" in j["headline"]:
+            # extract the forecasted sunlight information from the news item's body
+            # and add it to the InfoDic dictionary with a key that
+
+
+            InfoDic["Sunlight forecast at"+str(textAnalysis.returnDay(j["headline"]))+" at tick "+str(j["tick"])]=textAnalysis.returnTwoNums(j["body"],"between","sunlight")
+        
+    return(InfoDic)      
+
+
     
     
         
     
     
 def streamElements(app, **shared_data):
-    global previous_news
-    global newsevent
-    while True:
-        bidask = app.getSecuritiesBook("NG")
-        
-        bid = float(bidask["asks"].iloc[0]["price"])
-        ask= float(bidask["bids"].iloc[0]["price"])
-        new = app.getNews()
-        
-        print(bid,ask)
-        print(new)
+    global new
+    bidask = app.getSecuritiesBook("NG")
+    bid = float(bidask["asks"].iloc[0]["price"])
+    ask= float(bidask["bids"].iloc[0]["price"])
+    new = app.getNews()
+    
+    print(newsInfoFounder(new))
+    
+   
+    
+    
         #number = float(bidask["asks"].iloc[0]["price"])
 
 
@@ -68,8 +174,8 @@ def streamElements(app, **shared_data):
 
 def main(app):
     while True:
-        time.sleep(0.1)
-        securities = app.getTradingLimits()
+        time.sleep(0.001)
+        streamElements(app)
         
    
         
